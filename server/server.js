59a063e0bf189f7c2af4ee1327d2ec6d553cfc95
Max Schedin <max@adoreyou.se>
2010-12-08 00:02:04 +0100

    Added server by socket.io

---
+++ server/server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var io = require('./lib/socket.io')
+
+server = http.createServer(function(req, res){
+    // your normal server code
+    res.writeHeader(200, {'Content-Type': 'text/html'});
+    res.writeBody('<h1>Space is the limit</h1>');
+    res.finish();
+});
+
+server.listen(9090);
+
+var socket = io.listen(server, { transports : ['websocket'], port : 9090});
+
+var clients = [];
+
+socket.on('connection', function(client){
+	
+  console.log("Client connected");
+
+  client.on('message', function(e){
+	console.log("Message", e);
+	socket.broadcast(e);
+  })
+
+  client.on('disconnect', function(){
+	console.log("Client Disconnected");
+
+  })
+});
